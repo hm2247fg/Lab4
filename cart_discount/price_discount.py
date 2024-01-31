737734baeb3ba4e39d84d2ee6590253efbf6aba5
My name <me@email.com>
2024-01-31 13:21:16 -0600

    Updated comments for the programs

--- cart_discount/price_discount.py
+++ cart_discount/price_discount.py
@@ -10,11 +10,11 @@ def discount(item_prices):
     Example: if this function is called with a list of [10, 4, 20] then return 4.
     """
 
-    if len(item_prices) > 0:  # to avoid error empty list
-        min_value = item_prices[0]
-        for i in item_prices:
-            if i < min_value:
-                min_value = i
+    if len(item_prices) > 0:  # added to avoid errors if you try to find the minimum value in an empty list
+        min_value = item_prices[0]  # initializes the variable min_value with the first element of the list
+        for i in item_prices:  # for loop that iterates ove each element i in the list item_prices
+            if i < min_value:  # if statement that checks if i is smaller than the current minimum value
+                min_value = i  # if current value is smaller, it updates min value with the smaller amount
 
         return min_value
 
--- cart_discount/test_discount.py
+++ cart_discount/test_discount.py
@@ -10,26 +10,29 @@ class TestDiscount(TestCase):
         expected_discount = 4
         self.assertEqual(expected_discount, discount(prices))
 
-    def test_empty_list(self):  # tests for empty list
-        prices = []
-        self.assertIsNone(discount(prices))
+    """if the result is none, test will pass"""
+    def test_empty_list(self):  # defines test method for empty list
+        prices = []  # initializes an empty list named prices
+        self.assertIsNone(discount(prices))  # assertion - check if the result of calling discount with prices is none
 
-    def test_duplicate_prices(self):  # test for duplicate prices
-        prices = [10, 10, 10]
-        expected_discount = 10
+    def test_duplicate_prices(self):  # defines test method for duplicate prices
+        prices = [10, 10, 10]  # initializes a list named prices
+        expected_discount = 10  # sets the expected discount value
         self.assertEqual(expected_discount, discount(prices))
 
-    # def test_calculation(self):  """Test accuracy of calculation of discounted price when given an original price and a discount rate"""
-    #     prices = 100
-    #     discount_rate = 0.2  # 20% discount
-    #     expected_price = 80
-    #     assert discount(prices, discount_rate) == expected_price
+    """Test accuracy of calculation of discounted price when given an original price and a discount rate"""
+    def test_calculation(self):
+        prices = 100
+        discount_rate = 0.2  # 20% discount
+        expected_price = 80
+        assert discount(prices, discount_rate) == expected_price
 
-    # def test_discount_returns_original_when_rate_zero(self):  """Test if the function returns the original price when the discount rate is 0"""
-    #     prices = [10, 4, 100]
-    #     discount_rate = 0
-    #     expected_price = 100
-    #     assert discount(prices, discount_rate) == expected_price
+    """Test if the function returns the original price when the discount rate is 0"""
+    def test_discount_returns_original_when_rate_zero(self):
+        prices = [10, 4, 100]
+        discount_rate = 0
+        expected_price = 100
+        assert discount(prices, discount_rate) == expected_price
 
 
 if __name__ == '__main__':
